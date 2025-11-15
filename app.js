bb4059028c7c6b32cc0cd503bb2e26e5ddb79e17
Matheus Sena <matheussena02@gmail.com>
2025-11-15 18:12:12 -0300

    feat: incrementa funcionalidades para verificação das entradas do usuário >> - Adiciona a lógica para evitar que o usuário calcule as horas sem selecionar a carga horária necessária >> - Adiciona a lógica para evitar que o usuário deixe de inserir o número de dias faltados

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let cargaHoraria;
 let quantidadeHorasFaltasPossiveis;
 let quantidadeDiasFaltaPossiveis;
 let quantidadeDiasFaltaRestantes;
+let ultimoValorLido = "";
 
 let botao30h = document.querySelector("#botao30h");
 let botao60h = document.querySelector("#botao60h");
@@ -9,7 +10,6 @@ let botao90h = document.querySelector("#botao90h");
 let botao120h = document.querySelector("#botao120h");
 
 let entrada = document.querySelector("input");
-
 
 let botaoCalcular = document.querySelector("#botaocalcular");
 
@@ -38,6 +38,31 @@ botao120h.addEventListener("click", function(){
 })
 
 botaoCalcular.addEventListener("click", function(){
+    if(entrada.value == ""){
+        alert("É preciso inserir o números de dias de falta");
+    }
+    if(cargaHoraria == 0){
+        alert("É preciso digitar a carga horária");
+    }
     quantidadeDiasFaltaRestantes = quantidadeDiasFaltaPossiveis - entrada.value;
+    cargaHoraria = 0;
     console.log(quantidadeDiasFaltaRestantes);
 })
+
+entrada.addEventListener("keydown", function (event) {
+    const teclasPermitidas = [
+        "Backspace",
+        "Delete",
+        "ArrowLeft",
+        "ArrowRight",
+        "Tab"
+    ];
+
+    if (teclasPermitidas.includes(event.key)) {
+        return;
+    }
+
+    if (!/[0-9]/.test(event.key)) {
+        event.preventDefault();
+    }
+});
